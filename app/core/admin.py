108549a4c3e06f011ae81c44c79b612c78a54420
Yauheni Buhaev <buhaev.yauheni@gmail.com>
2022-07-19 01:23:20 +0300

    create admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin # noqa
+"""adding models in admin panel"""
 
+from django.contrib import admin # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """create good view of model"""
+
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_staff', 'is_superuser', ]
+    list_editable = ['is_staff', 'is_superuser', ]
+    fieldsets = (
+            (_('Main information'),
+                {'fields': ('email', 'password')}),
+            (_('permission'),
+                {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+            (_('important dates'),
+                {'fields': ('last_login',)}),
+            )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+            (
+                _('add new user'), {
+                    'classes': ('wide',),
+                    'fields': (
+                        'email',
+                        'name',
+                        'password1',
+                        'password2',
+                        'is_active',
+                        'is_staff',
+                        'is_superuser'
+                        )
+                    }),
+            )
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,47 @@
+"""tests for admin panel"""
+
+from django.test import (TestCase, Client)
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """test for django admin"""
+
+    def setUp(self):
+        """create user and client"""
+
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+                email='admin@test.com',
+                password='pass123', )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+                email='user@example.com',
+                password='testpass123',
+                name='test_user', )
+
+    def test_users_list(self):
+        """test that users are in list on the page"""
+
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """test of edit page"""
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test of creating user page"""
+
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
